9d117487bc8e46cc5fa403b3ec047881e08f18c1
Daniel Angst <dcangst@gmail.com>
2022-02-11 14:25:02 +0100

    order workspaces by last modified

--- alfred_open_vscode_project.sh
+++ alfred_open_vscode_project.sh
@@ -9,12 +9,14 @@ import json
 
 storage_path = osp.expanduser("~/Library/Application Support/Code/User/workspaceStorage")
 
-output = { "items": [] }
+workspaces = []
 for root, dirs, files in os.walk(storage_path):
     for file in files:
         if(file.endswith(".json")):
+            lastModified = os.path.getmtime(os.path.join(root,file))
             with open(os.path.join(root,file), "r", encoding="utf8") as file:
                 item = json.load(file)
+                
                 if "folder" in item:
                     path = item["folder"].replace("file://","")
                 elif "workspace" in item:
@@ -24,15 +26,19 @@ for root, dirs, files in os.walk(storage_path):
                 else:
                     next
                 title = parse.unquote(osp.basename(path))
-                output["items"].append({
-                    "title": title,
-                    "subtitle": path,
-                    "arg": path,
-                    "autocomplete": title,
-                    "icon": {
-                        "type": "fileicon",
-                        "path": path
-                    }
-                })
+                if os.path.exists(path):
+                    workspaces.append({
+                        "title": title,
+                        "subtitle": path,
+                        "arg": path,
+                        "autocomplete": title,
+                        "icon": {
+                            "type": "fileicon",
+                            "path": path
+                        },
+                        "sortOrder": lastModified
+                    })
+
+output = { "items": sorted(workspaces, key=lambda t: -t["sortOrder"])}
 
 print(json.dumps(output))
